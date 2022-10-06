f9935b176f030750f3b96c7a5d2edd5ad18e58c1
YuushaFr <92053553+YuushaFr@users.noreply.github.com>
2022-10-06 14:30:46 +0200

    Create salameche.java
    I made a salamèche :D

---
+++ salameche.java
@@ -0,0 +1,33 @@
+public class Salameche extends Feu{
+	private String espece;
+	
+	public Salameche(String nom, int pvmax, int attaque, int x, int y){
+		super(nom,pvmax,attaque,x,y);
+		espece="Salamèche";
+	}
+	
+	
+	public Salameche(String nom, int pvmax, int attaque) {
+		super(nom,pvmax,attaque);
+		espece="Carapuce";
+	}
+	
+	public Salameche(){
+		super();
+		espece="Salamèche";
+	}
+	
+	public String toString(){
+		return nom;
+	}
+	
+	public String getId(){
+		return espece+" "+nom+ " (" + pv + "/" + pvmax + "pv, " + attaque + " att, Niv. " + niveau +")";
+	}
+	
+	public void talent (){
+		pv = pv + (int)(pvmax*0.1);
+		if (pv > pvmax)
+			pv = pvmax;
+	}
+}
