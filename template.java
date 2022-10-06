2707588937c664fbaa649e480c39dab8d1665ae4
Knackie <43431712+Knackie@users.noreply.github.com>
2022-10-06 13:32:27 +0200

    Create template.java

---
+++ template.java
@@ -0,0 +1,33 @@
+public class Pokemon extends Type{
+	private String espece;
+	
+	public Pokemon(String nom, int pvmax, int attaque, int x, int y){
+		super(nom,pvmax,attaque,x,y);
+		espece="";
+	}
+	
+	
+	public Pokemon(String nom, int pvmax, int attaque) {
+		super(nom,pvmax,attaque);
+		espece="";
+	}
+	
+	public Pokemon(){
+		super();
+		espece="";
+	}
+	
+	public String toString(){
+		return nom;
+	}
+	
+	public String getId(){
+		return espece+" "+nom+ " (" + pv + "/" + pvmax + "pv, " + attaque + " att, Niv. " + niveau +")";
+	}
+	
+	public void talent (){
+		pv = pv + (int)(pvmax*0.1);
+		if (pv > pvmax)
+			pv = pvmax;
+	}
+}
